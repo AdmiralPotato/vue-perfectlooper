287544e5c62326cebee09a2baf77aa198804de22
Admiral Potato <admiral@nuclearpixel.com>
2016-12-11 06:27:22 -0700

    adding anything interactive at all

--- app.js
+++ app.js
@@ -1,44 +1,113 @@
+let state = {
+	activePage: 'videos',
+	activeVideo: undefined
+};
+
 Vue.component(
-    'list-item',
-    {
-        props: {
-            title: String,
-            image: String
-        },
-        template: `
-            <div class="col-xs-6 col-sm-4 col-md-3">
-                <div class="box">
-                    <a :href="image">
-                        <span class="thumb"></span>
-                        <span class="titleHolder hidden-xs-down"><span class="title">{{title}}</span>
-                    </a>
-                </div>
-            </div>
-        `
-    }
+	'contact',
+	{
+		template: `
+		<div class="row">
+			<div class="box col-xs-12 col-lg-offset-2 col-lg-8"><h1>contact</h1></div>
+		</div>
+		`
+	}
 );
 
 Vue.component(
-    'list',
-    {
-        props: ['videoList'],
-        template: `
-            <div class="row">
-                <list-item
-                    v-for="video in videoList"
-                    :title="video.title"
-                    :image="video.name" />
-            </div>
-        `
-    }
+	'about',
+	{
+		template: `
+		<div class="row">
+			<div class="box col-xs-12 col-lg-offset-2 col-lg-8"><h1>about</h1></div>
+		</div>
+		`
+	}
 );
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        videoList: videoList
-    },
-    template: `
-        <list :videoList="videoList"></list>
-    `
+Vue.component(
+	'videos',
+	{
+		props: ['state'],
+		template: `
+			<div>
+				<video-detail v-if="state.activeVideo" :video="state.activeVideo" />
+				<list v-if="!state.activeVideo" />
+			</div>
+		`
+	}
+);
+
+Vue.component(
+	'video-detail',
+	{
+		props: {
+			video: Object
+		},
+		template: `
+			<div class="row">
+				<div class="box col-xs-12 col-lg-offset-2 col-lg-8 video">wat up video goes here</div>
+				<div class="box col-xs-12 col-lg-offset-2 col-lg-8"><h1>{{video.title}}</h1></div>
+				<div class="box col-xs-12 col-lg-offset-2 col-lg-8" v-html="video.description" />
+			</div>
+		`
+	}
+);
+
+Vue.component(
+	'list',
+	{
+		data: function(){
+			return {
+				videoList: videoList
+			};
+		},
+		template: `
+			<div class="row">
+				<list-item
+					v-for="video in videoList"
+					:video="video" />
+			</div>
+		`
+	}
+);
+
+Vue.component(
+	'list-item',
+	{
+		props: {
+			video: Object
+		},
+		methods: {
+			changeImage: function(){
+				state.activeVideo = this.video
+			}
+		},
+		template: `
+			<div class="col-xs-6 col-sm-4 col-md-3">
+				<div class="box">
+					<a @click="changeImage">
+						<span class="thumb"></span>
+						<span class="titleHolder hidden-xs-down"><span class="title">{{video.title}}</span>
+					</a>
+				</div>
+			</div>
+		`
+	}
+);
+
+let app = new Vue({
+	el: '#app',
+	data: {
+		state: state
+	},
+	methods: {
+		navigate: function(destination){
+			state.activePage = destination;
+			state.activeVideo = undefined;
+		}
+	},
+	template: `
+		<component :is="state.activePage" :state="state"></component>
+	`
 });
--- data.js
+++ data.js
@@ -1,10 +1,71 @@
-var videoList = [
-    {title: 'Candy Links - Rainbow', name: 'candy_links_rainbow-48-jpg', description: ''},
-    {title: 'Candy Links', name: 'candy_links-48-jpg', description: ''},
-    {title: 'Chocolate Hearts', name: 'chocolate_hearts-48-jpg', description: ''},
-    {title: 'Hella Hearts', name: 'hella_hearts-48-jpg', description: ''},
-    {title: 'Hexagonal Hard Candy', name: 'hexagonal_hard_candy-96-jpg', description: ''},
-    {title: 'Honey Swarm', name: 'honey_swarm_d-48-jpg', description: ''},
-    {title: 'Metal Eyes', name: 'metal_eyes-24-jpg', description: ''},
-    {title: 'Networking', name: 'networking-48-jpg', description: ''}
+let ipsum = `
+<h1>HTML Ipsum Presents</h1>
+
+<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+<h2>Header Level 2</h2>
+
+<ol>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ol>
+
+<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+
+<h3>Header Level 3</h3>
+
+<ul>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ul>
+
+<pre><code>
+#header h1 a {
+  display: block;
+  width: 300px;
+  height: 80px;
+}
+</code></pre>
+`;
+let videoList = [
+	{
+		title: 'Candy Links - Rainbow',
+		name: 'candy_links_rainbow-48-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Candy Links',
+		name: 'candy_links-48-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Chocolate Hearts',
+		name: 'chocolate_hearts-48-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Hella Hearts',
+		name: 'hella_hearts-48-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Hexagonal Hard Candy',
+		name: 'hexagonal_hard_candy-96-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Honey Swarm',
+		name: 'honey_swarm_d-48-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Metal Eyes',
+		name: 'metal_eyes-24-jpg',
+		description: ipsum
+	},
+	{
+		title: 'Networking',
+		name: 'networking-48-jpg',
+		description: ipsum
+	}
 ];
