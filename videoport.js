cce94d71c2e888ff46f2659fb890f2aa985a3183
Admiral Potato <admiral@nuclearpixel.com>
2017-03-25 21:15:15 -0600

    Add basic playback, separate Decode & Buffer tasks from Videoport

--- videoport.js
+++ videoport.js
@@ -19,9 +19,11 @@ Vue.component(
 			return {
 				width: 0,
 				height: 0,
-				go: false,
-				loading: false,
-				loaded: false,
+				started: false,
+				playing: false,
+				loaded: 0,
+				decoded: 0,
+				ready: false,
 				statusMessage: ''
 			}
 		},
@@ -30,49 +32,40 @@ Vue.component(
 		},
 		mounted: function() {
 			let t = this;
-			let videoport = videoportMap[t.video.name] || new Videoport(t.video);
-			videoport.attachVue(t, t.$refs.canvas);
-			this.videoport = videoport;
+			this.videoport = new Videoport(t.video, t, t.$refs.canvas);
 		},
 		beforeMount: function () {
 			document.addEventListener('resize', resizeWindowEventHandler);
 			window.addEventListener('resize', resizeWindowEventHandler);
 		},
 		beforeDestroy: function () {
-			this.videoport.removeVue();
+			this.videoport.die();
 			document.removeEventListener('resize', resizeWindowEventHandler);
 			window.removeEventListener('resize', resizeWindowEventHandler);
 		},
 		methods: {
 			toggle: function(event){
-				this.go = !this.go;
-				this.videoport.toggle(this.go);
-			}
-		},
-		computed: {
-			symbolState: function () {
-				let t = this;
-				let symbol = 'play';
-				if(t.go){
-					symbol = 'pause';
-				} else if(t.loading) {
-					symbol = 'loading';
-				}
-				return symbol;
+				let v = this;
+				v.started = true;
+				v.playing = !v.playing;
+				v.videoport.setPlay(v.playing);
 			}
 		},
 		template: `
-			<div class="videoport noSelect">
+			<div class="videoport noSelect" @click="toggle">
 				<canvas
 					ref="canvas"
 					:width="width"
 					:height="height"
 					/>
-				<div v-if="!loaded" class="overlay" @click="toggle">
-					<img v-if="!go" :src="thumbUrl(video)" />
-					<div class="statusMessage">{{statusMessage}}</div>
-					<video-status-icon :type="symbolState" />
-				</div>
+					<transition-group name="fade">
+						<div key="a" v-if="!ready" class="overlay transition">
+							<img v-if="!decoded" :src="thumbUrl(video)" />
+							<div class="statusMessage">{{statusMessage}} - Loaded: {{loaded.toFixed(2)}} - Decoded: {{decoded.toFixed(2)}}</div>
+						</div>
+						<video-status-icon key="b" v-if="!ready && started" type="loading" class="rotating" />
+						<video-status-icon key="c" v-if="!started || !playing && ready" type="play" />
+					</transition-group>
 			</div>
 		`
 	}
@@ -92,80 +85,161 @@ Vue.component(
 			};
 		},
 		template: `
-			<svg class="video-status-icon" viewBox="0 0 128 128">
+			<svg class="video-status-icon transition" viewBox="0 0 128 128">
 				<path :d="shapes[type]" />
 			</svg>
 		`
 	}
 );
 
-let videoportMap = {};
+let arrayRemove = function(array, item){
+	let index = array.indexOf(item);
+	if(index !== -1){
+		array.splice(index, 1);
+	}
+	return array;
+};
+
 let videoportList = [];
 
-let Videoport = function(video){
+let Videoport = function(video, vue, canvas){
 	let p = this;
 	p.video = video;
-	p.url = mixinAddresses.methods.videoUrl(video);
-	p.loaded = false;
-	p.loading = false;
+	p.vue = vue;
+	p.canvas = canvas;
+	p.context = canvas.getContext('2d');
 	p.shouldPlay = false;
-	p.bufferCanvasList = [];
-	p.width = 0;
-	p.height = 0;
+	p.ready = false;
+	p.playOffset = 0;
+	p.sizeWindow();
+	p.sourceBuffer = decodedFrameBufferMap[p.video.name] || new DecodedFrameBuffer(p.video);
+	p.sourceBuffer.addVideoport(p);
 
-	p.player = new libde265.RawPlayer(document.createElement('canvas'));
-	p.player.set_status_callback(function(msg, fps) {
-		if (msg !== 'fps') {
-			p.vue.statusMessage = msg;
-		}
-	});
-	p.player.addVideoPort(p);
-	videoportMap[video.name] = p;
+	videoportList.push(p);
 };
 
 Videoport.prototype = {
-	attachVue: function(vue, canvas){
-		let p = this;
-		p.vue = vue;
-		p.canvas = canvas;
-		p.context = canvas.getContext('2d');
-		videoportList.push(p);
-
-		p.vue.loaded = p.loaded;
-		p.vue.loading = p.loading;
-		p.sizeWindow();
+	fps: 24,
+	die: function(){
+		this.sourceBuffer.removeVideoport(this);
+		arrayRemove(videoportList, this);
 	},
-	removeVue: function(){
+	setPlay: function(shouldPlay){
+		this.shouldPlay = shouldPlay;
+		this.sourceBuffer.load();
+	},
+	render: function (time) {
 		let p = this;
-		p.vue = undefined;
-		p.canvas = undefined;
-		p.context = undefined;
-		let index = videoportList.indexOf(p);
-		if(index !== -1){
-			videoportList.splice(index, 1);
+		if(p.shouldPlay && p.ready){
+			let delta = time - (p.lastTimeSample || 0);
+			p.playOffset += delta;
+			p.setFrameByTime(p.playOffset);
 		}
+		p.lastTimeSample = time;
 	},
-	toggle: function(go){
-		this.shouldPlay = go;
-		this.load();
-	},
-	load: function () {
+	setFrameByTime: function(time){
 		let p = this;
-		if(!p.loaded || !p.loading){
-			p.player.playback(p.url);
+		let frames = p.sourceBuffer.frameCount;
+		let currentFrame = Math.floor(time / 1000 / (frames / p.fps) * frames) % frames;
+		if(currentFrame !== p.prevFrame){
+			let sourceImage = p.sourceBuffer.canvasList[currentFrame];
+			p.context.drawImage(sourceImage, 0, 0, p.width, p.height);
+			p.prevFrame = currentFrame;
 		}
-	},
-	render: function () {
-		//TODO: anything at all here
 	},
 	sizeWindow: function () {
 		let p = this;
-		let ratio = window.devicePixelRatio || 1;
+		let ratio = window['devicePixelRatio'] || 1;
 		p.width = p.canvas.clientWidth * ratio;
 		p.height = p.canvas.clientHeight * ratio;
 		p.vue.width = p.width;
 		p.vue.height = p.height;
 	},
+	handleBufferUpdate: function(){
+		let p = this;
+		let b = p.sourceBuffer;
+		let vue = p.vue;
+		vue.statusMessage = b.status;
+		vue.loaded = b.loaded;
+		vue.decoded = b.decoded;
+		vue.ready = b.ready;
+		p.ready = b.ready;
+		let imageIndex = b.ready ? 0 : b.canvasList.length - 1;
+		let image = b.canvasList[imageIndex];
+		if(image){
+			//I guess you can't render to a context the instant it's created?
+			requestAnimationFrame(function(){
+				p.context.drawImage(image, 0, 0, p.width, p.height);
+			});
+		}
+	}
+};
+
+let decodedFrameBufferMap = {};
+
+let DecodedFrameBuffer = function(video){
+	let b = this;
+	b.url = mixinAddresses.methods.videoUrl(video);
+	b.frameCount = parseInt(video.name.split('-').pop(), 10);
+	b.started = false;
+	b.loaded = 0;
+	b.decoded = 0;
+	b.ready = false;
+	b.status = 'Not loaded';
+	b.canvasList = [];
+	b.videoportList = [];
+	b.decoder = new libde265.RawPlayer(document.createElement('canvas'));
+	b.decoder.addFrameBuffer(b);
+	decodedFrameBufferMap[video.name] = b;
+};
+
+DecodedFrameBuffer.prototype = {
+	addVideoport: function(p){
+		this.videoportList.push(p);
+		this.updateVideoports();
+	},
+	removeVideoport: function(p){
+		arrayRemove(this.videoportList, p);
+	},
+	updateVideoports: function () {
+		let b = this;
+		b.videoportList.forEach(function(p){
+			p.handleBufferUpdate();
+		});
+	},
+	load: function () {
+		let b = this;
+		if(!b.started){
+			b.status = 'Loading';
+			b.started = true;
+			b.decoder.playback(b.url);
+			this.updateVideoports();
+		}
+	},
+	handleDecoderLoadComplete: function(){
+		this.status = 'Loading complete';
+		this.loaded = 1;
+		this.updateVideoports();
+	},
+	handleDecoderDecodeStart: function(){
+		this.status = 'Decoding started';
+		this.updateVideoports();
+	},
+	handleDecoderFrame: function(frameCanvas){
+		let b = this;
+		let framesLoaded = b.canvasList.push(frameCanvas);
+		b.decoded = framesLoaded / b.frameCount;
+		b.lastStoredFrame = frameCanvas;
+		b.status = `Decoded ${framesLoaded} / ${b.frameCount} frames`;
+		this.updateVideoports();
+	},
+	handleDecoderFinish: function(){
+		this.status = `Ready; Loaded & Decoded`;
+		this.ready = true;
+		this.decoder = null;
+		this.decoded = 1;
+		this.updateVideoports();
+	}
 };
 
 let resizeWindowEventHandler = function () {
@@ -198,29 +272,38 @@ let render = function (time){
 start();
 
 let hevcImageInterceptor = function(libde265Image){
-	let player = this;
-	let videoport = player.videoport;
+	let decoder = this;
+	let decodedFrameBuffer = decoder.decodedFrameBuffer;
 	let bufferCanvas = document.createElement('canvas');
 	let bufferContext = bufferCanvas.getContext('2d');
 	let w = libde265Image.get_width();
 	let h = libde265Image.get_height();
-	if (w !== bufferCanvas.width || h !== bufferCanvas.height || !player.image_data) {
-		bufferCanvas.width = w;
-		bufferCanvas.height = h;
-		bufferContext.fillStyle = '#000';
-		bufferContext.fillRect(0,0,w,h);
-		player.image_data = bufferContext.createImageData(w, h);
-	}
-	libde265Image.display(player.image_data, function(display_image_data) {
+	bufferCanvas.width = w;
+	bufferCanvas.height = h;
+	bufferContext.fillStyle = '#000';
+	bufferContext.fillRect(0,0,w,h);
+	decoder.image_data = bufferContext.createImageData(w, h);
+	libde265Image.display(decoder.image_data, function(display_image_data) {
 		bufferContext.putImageData(display_image_data, 0, 0);
-		videoport.bufferCanvasList.push(bufferCanvas);
-		videoport.context.drawImage(bufferCanvas, 0, 0);
-		if(!player.running){
-			videoport.loaded = true;
+		decodedFrameBuffer.handleDecoderFrame(bufferCanvas);
+		if(!decoder.running){
+			decoder.decodedFrameBuffer.handleDecoderFinish();
 		}
 	});
 };
-libde265.RawPlayer.prototype.addVideoPort = function(videoport){
-	this.videoport = videoport;
+
+libde265.RawPlayer.prototype.handlerMap = {
+	loading: 'LoadComplete',
+	initializing: 'DecodeStart'
+};
+libde265.RawPlayer.prototype.addFrameBuffer = function(decodedFrameBuffer){
+	let decoder = this;
+	decoder.decodedFrameBuffer = decodedFrameBuffer;
+	decoder.set_status_callback(function(msg) {
+		let handler = decoder.handlerMap[msg];
+		if (handler) {
+			decodedFrameBuffer['handleDecoder' + handler]();
+		}
+	});
 };
 libde265.RawPlayer.prototype._display_image = hevcImageInterceptor;
