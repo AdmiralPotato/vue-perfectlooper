162ca67af6c4efab1c1b31dcfff619005f702517
Admiral Potato <admiral@nuclearpixel.com>
2017-04-13 07:32:56 -0600

    Replace Bootstrap w/ custom, Make `video-list`, `video-detail` top level - Mobile view loses box corners, video stretches to edge

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let state = {
-	activePage: 'videos',
+	activePage: 'video-list',
 	activeVideo: undefined
 };
 
@@ -9,10 +9,8 @@ Vue.component(
 	'contact',
 	{
 		template: `
-		<div class="row" v-once>
-			<div class="col-xs-12 offset-lg-2 col-lg-8">
-				<div class="box forText"><h1>Contact</h1></div>
-			</div>
+		<div class="contact" v-once>
+			<div class="box forText"><h1>Contact</h1></div>
 		</div>
 		`
 	}
@@ -22,30 +20,15 @@ Vue.component(
 	'about',
 	{
 		template: `
-		<div class="row" v-once>
-			<div class="col-xs-12 offset-lg-2 col-lg-8">
-				<div class="box forText">
-					<h1>About</h1>
-					<p>Hi. My name is <a href="http://nuclearpixel.com/">Admiral Potato</a>. Welcome to my perfectly-looping Video Portfolio. I put a lot of work into making online experience looks as good as it does, so let's make it sound impressive with some buzzwords. This is a first-of-its-kind Marble Friendly Intersite Webbernet Wapp with Lazor Synthesized Virtual Shadow Dog Crumpitulation.</p>
-					<p>I made this because I wanted a place to post my perfectly looping animations in a fullscreen format with a minimum of visually offensive compression artifacts - and most importantly, I wanted my animations to loop perfectly. No existing video hosting service met my standards.</p>
-					<h2>License - How you may use this site and the content</h2>
-					<p>The content on this site is licensed under the <a href="https://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International</a>.</p>
-				</div>
+		<div class="about" v-once>
+			<div class="box forText">
+				<h1>About</h1>
+				<p>Hi. My name is <a href="http://nuclearpixel.com/">Admiral Potato</a>. Welcome to my perfectly-looping Video Portfolio. I put a lot of work into making online experience looks as good as it does, so let's make it sound impressive with some buzzwords. This is a first-of-its-kind Marble Friendly Intersite Webbernet Wapp with Lazor Synthesized Virtual Shadow Dog Crumpitulation.</p>
+				<p>I made this because I wanted a place to post my perfectly looping animations in a fullscreen format with a minimum of visually offensive compression artifacts - and most importantly, I wanted my animations to loop perfectly. No existing video hosting service met my standards.</p>
+				<h2>License - How you may use this site and the content</h2>
+				<p>The content on this site is licensed under the <a href="https://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International</a>.</p>
 			</div>
 		</div>
-		`
-	}
-);
-
-Vue.component(
-	'videos',
-	{
-		props: ['state'],
-		template: `
-			<div>
-				<video-detail v-if="state.activeVideo" :video="state.activeVideo" />
-				<list v-if="!state.activeVideo" />
-			</div>
 		`
 	}
 );
@@ -54,28 +37,32 @@ Vue.component(
 	'video-detail',
 	{
 		props: {
-			video: Object
+			state: Object
+		},
+		created: function(){
+			this.video = this.state.activeVideo;
+			if(!this.video){
+				state.activePage = 'video-list';
+			}
 		},
 		template: `
-			<div class="row">
-				<div class="col-xs-12 offset-lg-1 col-lg-10">
-					<div class="box">
-						<div class="videoHolder">
-							<div class="video">
-								<videoport :video="video" />
-							</div>
+			<div class="video-detail">
+				<div class="box forVideo">
+					<div class="videoHolder">
+						<div class="video">
+							<videoport :video="video" />
 						</div>
 					</div>
-					<div class="box forText"><h1>{{video.title}}</h1></div>
-					<div class="box forText" v-html="video.description"></div>
 				</div>
+				<div class="box forText"><h1>{{video.title}}</h1></div>
+				<div class="box forText" v-html="video.description"></div>
 			</div>
 		`
 	}
 );
 
 Vue.component(
-	'list',
+	'video-list',
 	{
 		data: function(){
 			return {
@@ -83,8 +70,8 @@ Vue.component(
 			};
 		},
 		template: `
-			<div class="row">
-				<list-item
+			<div class="video-list noSelect">
+				<video-list-item
 					v-for="video in videoList"
 					:video="video"
 					:key="video.name" />
@@ -94,7 +81,7 @@ Vue.component(
 );
 
 Vue.component(
-	'list-item',
+	'video-list-item',
 	{
 		mixins: [mixinAddresses],
 		props: {
@@ -103,14 +90,17 @@ Vue.component(
 		methods: {
 			navigateToVideo: function(){
 				state.activeVideo = this.video;
+				state.activePage = 'video-detail';
 				window.scrollTo(0, 0);
 			}
 		},
 		template: `
-			<div class="col-xs-6 col-sm-4 col-md-3 noSelect">
-				<a class="box" @click="navigateToVideo" tabindex="0">
-					<span class="thumbHolder"><span class="thumb"><img :src="thumbUrl(video)" /></span></span>
-					<span class="titleHolder hidden-xs-down"><span class="title">{{video.title}}</span></span>
+			<div class="video-list-item">
+				<a @click="navigateToVideo" tabindex="0">
+					<span class="box">
+						<span class="thumbHolder"><span class="thumb"><img :src="thumbUrl(video)" /></span></span>
+						<span class="titleHolder"><span class="title">{{video.title}}</span></span>
+					</span>
 				</a>
 			</div>
 		`
@@ -133,14 +123,18 @@ let app = new Vue({
 			<div class="backgroundImage"></div>
 			<div class="dots"></div>
 			<div class="container">
-				<header class="row">
-					<div class="col-xs-12 col-md-9">
-						<h1 tabindex="0" @click="navigate('videos');"><em>Video</em>.NuclearPixel.com</h1>
+				<header>
+					<div class="brand">
+						<h1 tabindex="0" @click="navigate('video-list');"><em>Video</em>.NuclearPixel.com</h1>
 						<h4>v0.0.1</h4>
 					</div>
-					<div class="nav col-xs-4 col-md-1"><a tabindex="0" @click="navigate('videos');">Videos</a></div>
-					<div class="nav col-xs-4 col-md-1"><a tabindex="0" @click="navigate('about');">About</a></div>
-					<div class="nav col-xs-4 col-md-1"><a tabindex="0" @click="navigate('contact');">Contact</a></div>
+					<nav>
+						<ul>
+							<li><a tabindex="0" @click="navigate('video-list')">Videos</a></li>
+							<li><a tabindex="0" @click="navigate('about')">About</a></li>
+							<li><a tabindex="0" @click="navigate('contact')">Contact</a></li>
+						</ul>
+					</nav>
 				</header>
 				<transition
 					name="fadeOutRight"
--- data.js
+++ data.js
@@ -1,9 +1,7 @@
 "use strict";
 
 let ipsum = `
-<h1>HTML Ipsum Presents</h1>
-
-<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+<p><strong>HTML Ipsum Presents - Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 
 <h2>Header Level 2</h2>
 
@@ -21,13 +19,11 @@ let ipsum = `
    <li>Aliquam tincidunt mauris eu risus.</li>
 </ul>
 
-<pre><code>
-#header h1 a {
+<pre><code>#header h1 a {
   display: block;
   width: 300px;
   height: 80px;
-}
-</code></pre>
+}</code></pre>
 `;
 let videoList = [
 	{
