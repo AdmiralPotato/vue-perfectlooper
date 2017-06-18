d0b60a86f0f2ce81b8d51837d1f4bdca570dc464
Admiral Potato <admiral@nuclearpixel.com>
2017-06-18 05:24:40 -0600

    Loading all frames in parallel! Faster load, Delayed/Clumped UI updates

--- decoder-image.js
+++ decoder-image.js
@@ -4,36 +4,35 @@ let DecoderImage = function (decodedFrameBuffer) {
 	let decoder = this;
 	decoder.decodedFrameBuffer = decodedFrameBuffer;
 	decoder.frameCount = decoder.decodedFrameBuffer.frameCount;
+	decoder.framesLoaded = 0;
 };
 
 DecoderImage.prototype = {
 	startLoad: function (url) {
 		let decoder = this;
 		decoder.src = url;
-		let frameCount = decoder.decodedFrameBuffer.frameCount;
-		let frame = 0;
-		let imageNameList = [];
-		while(frameCount > frame) {
-			imageNameList.push(decoder.getImagePath(frame++));
-		}
-		let nextIndex = 0;
 		decoder.decodedFrameBuffer.handleDecoderLoadStart();
-		decoder.decodedFrameBuffer.handleDecoderDecodeStart();
-		decoder.running = true;
-		let getNextImage = function () {
-			if(nextIndex < frameCount){
+		for (let i = 0; i < decoder.frameCount; i++) {
+			let frameIndex = i;
+			let src = decoder.getImagePath(frameIndex);
+			let getImage = function () {
 				let image = new Image();
-				let frameIndex = nextIndex;
-				image.addEventListener('load', function(){
-					decoder.decodedFrameBuffer.handleDecoderFrame(image, frameIndex);
-					nextIndex++;
-					getNextImage();
+				image.addEventListener('load', function(e){
+					if(!decoder.framesLoaded){
+						decoder.decodedFrameBuffer.handleDecoderDecodeStart();
+					}
+					decoder.framesLoaded += 1;
+					requestAnimationFrame(function () {
+						decoder.decodedFrameBuffer.handleDecoderFrame(image, frameIndex);
+					});
 				});
-				image.addEventListener('error', getNextImage);
-				image.src = imageNameList[nextIndex];
-			}
-		};
-		getNextImage();
+				image.addEventListener('error', function(){
+					setTimeout(getImage, 100);
+				});
+				image.src = src;
+			};
+			getImage();
+		}
 	},
 	getImagePath: function(frameIndex){
 		let str = (frameIndex + 1).toString();
