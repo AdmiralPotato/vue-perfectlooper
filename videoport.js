f08b1e676c2fa4a1e4d610213754cfe83312274c
Admiral Potato <admiral@nuclearpixel.com>
2017-07-11 02:33:20 -0600

    Giving all interactive elements in the Videoport an :active color

--- videoport.js
+++ videoport.js
@@ -255,7 +255,7 @@ Vue.component(
 			<svg class="video-play-icon" viewBox="0 0 128 128">
 				<g class="fade">
 					<path :d="shape" class="stroke" />
-					<path :d="shape" class="fill" />
+					<path :d="shape" fill="currentColor" />
 				</g>
 			</svg>
 		`
@@ -379,9 +379,9 @@ Vue.component(
 		template: `
 			<svg class="video-controller-defs" style="display: none;">
 				<defs>
-					<path id="screen-line" class="stroke screen-line" d="M3.5,3.5l5,5"/>
-					<path id="full-triangle" class="fill" d="M10,9V4.41421a1,1,0,0,0-1.70711-.70711L3.70711,8.29289A1,1,0,0,0,4.41421,10H9A1,1,0,0,0,10,9Z"/>
-					<path id="exit-triangle" class="fill" d="M8.29289,3.70711,3.70711,8.29289A1,1,0,0,1,2,7.58579V3A1,1,0,0,1,3,2H7.58579A1,1,0,0,1,8.29289,3.70711Z"/>
+					<path id="screen-line" class="stroke screen-line" stroke="currentColor" d="M3.5,3.5l5,5"/>
+					<path id="full-triangle" fill="currentColor" d="M10,9V4.41421a1,1,0,0,0-1.70711-.70711L3.70711,8.29289A1,1,0,0,0,4.41421,10H9A1,1,0,0,0,10,9Z"/>
+					<path id="exit-triangle" fill="currentColor" d="M8.29289,3.70711,3.70711,8.29289A1,1,0,0,1,2,7.58579V3A1,1,0,0,1,3,2H7.58579A1,1,0,0,1,8.29289,3.70711Z"/>
 					<g id="full-corner">
 						<use xlink:href="#screen-line" />
 						<use xlink:href="#full-triangle" />
@@ -406,13 +406,13 @@ Vue.component(
 							<use xlink:href="#exit-corner" transform="rotate(-90)"/>
 						</g>
 					</g>
-					<path id="icon-play" class="fill" d="M17,15.33975V32.66025a2,2,0,0,0,3,1.73205l15-8.66025a2,2,0,0,0,0-3.4641L20,13.6077A2,2,0,0,0,17,15.33975Z"/>
+					<path id="icon-play" fill="currentColor" d="M17,15.33975V32.66025a2,2,0,0,0,3,1.73205l15-8.66025a2,2,0,0,0,0-3.4641L20,13.6077A2,2,0,0,0,17,15.33975Z"/>
 					<g id="icon-loading">
-						<path class="fill" d="M28.56066,26.56066l4.37868,4.37868a1.5,1.5,0,0,0,2.12132,0l4.37868-4.37868A1.5,1.5,0,0,0,38.37868,24H29.62132A1.5,1.5,0,0,0,28.56066,26.56066Z"/>
-						<path class="stroke loading" d="M24,34A10,10,0,1,1,34,24"/>
+						<path fill="currentColor" d="M28.56066,26.56066l4.37868,4.37868a1.5,1.5,0,0,0,2.12132,0l4.37868-4.37868A1.5,1.5,0,0,0,38.37868,24H29.62132A1.5,1.5,0,0,0,28.56066,26.56066Z"/>
+						<path class="stroke loading" stroke="currentColor" d="M24,34A10,10,0,1,1,34,24"/>
 					</g>
-					<path id="icon-pause" class="fill" d="M19,34H17a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2h2a2,2,0,0,1,2,2V32A2,2,0,0,1,19,34Zm12,0H29a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2h2a2,2,0,0,1,2,2V32A2,2,0,0,1,31,34Z"/>
-					<path id="icon-advance" class="fill" d="M26.125,16.85529l9,5.19615a2.25,2.25,0,0,1,0,3.89711l-9,5.19615a2.25,2.25,0,0,1-3.375-1.94856V18.80385A2.25,2.25,0,0,1,26.125,16.85529ZM17.75,29.5v-11a2,2,0,0,0-2-2h-.5a2,2,0,0,0-2,2v11a2,2,0,0,0,2,2h.5A2,2,0,0,0,17.75,29.5Z"/>
+					<path id="icon-pause" fill="currentColor" d="M19,34H17a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2h2a2,2,0,0,1,2,2V32A2,2,0,0,1,19,34Zm12,0H29a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2h2a2,2,0,0,1,2,2V32A2,2,0,0,1,31,34Z"/>
+					<path id="icon-advance" fill="currentColor" d="M26.125,16.85529l9,5.19615a2.25,2.25,0,0,1,0,3.89711l-9,5.19615a2.25,2.25,0,0,1-3.375-1.94856V18.80385A2.25,2.25,0,0,1,26.125,16.85529ZM17.75,29.5v-11a2,2,0,0,0-2-2h-.5a2,2,0,0,0-2,2v11a2,2,0,0,0,2,2h.5A2,2,0,0,0,17.75,29.5Z"/>
 					<rect id="button-click-mask" opacity="0" x="2" y="2" width="44" height="44" />
 				</defs>
 			</svg>
